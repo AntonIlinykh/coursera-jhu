d560eb88d9bebdcf58dd8227a0e94862bf4bc2a5
Антон Ильиных < >
2017-03-17 18:58:52 +0700

    menu

--- games/js/common.js
+++ games/js/common.js
@@ -1,5 +1,54 @@
 $(function() {
 
+	var slideout = new Slideout({
+		'panel': document.getElementById('panel'),
+		'menu': document.getElementById('menu'),
+		'padding': 256,
+		'tolerance': 70
+	});
+	document.querySelector('.toggle-button').addEventListener('click', function() {
+			slideout.toggle();
+	});
+	var fixed = document.querySelector('.navbar-header');
+
+	slideout.on('translate', function(translated) {
+	  fixed.style.transform = 'translateX(' + translated + 'px)';
+	});
+
+	slideout.on('beforeopen', function () {
+	  fixed.style.transition = 'transform 300ms ease';
+	  fixed.style.transform = 'translateX(256px)';
+	});
+
+	slideout.on('beforeclose', function () {
+	  fixed.style.transition = 'transform 300ms ease';
+	  fixed.style.transform = 'translateX(0px)';
+	});
+
+	slideout.on('open', function () {
+	  fixed.style.transition = '';
+	});
+
+	slideout.on('close', function () {
+	  fixed.style.transition = '';
+	});
+
+	function close(eve) {
+  eve.preventDefault();
+  slideout.close();
+}
+
+slideout
+  .on('beforeopen', function() {
+    this.panel.classList.add('panel-open');
+  })
+  .on('open', function() {
+    this.panel.addEventListener('click', close);
+  })
+  .on('beforeclose', function() {
+    this.panel.classList.remove('panel-open');
+    this.panel.removeEventListener('click', close);
+  });
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -7,6 +56,8 @@ $(function() {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
+
+	slideout.disableTouch();
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
