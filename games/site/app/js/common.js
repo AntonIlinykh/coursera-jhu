c69c16266dde12b2b61531531cf272d7d5958dca
Антон Ильиных < >
2017-03-25 09:11:06 +0700

    schedule

--- games/site/app/js/common.js
+++ games/site/app/js/common.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	//Slideout Menu
 	var slideout = new Slideout({
 		'panel': document.getElementById('panel'),
 		'menu': document.getElementById('menu'),
@@ -12,44 +13,47 @@ $(function() {
 	var fixed = document.querySelector('.navbar-header');
 
 	slideout.on('translate', function(translated) {
-	  fixed.style.transform = 'translateX(' + translated + 'px)';
+		fixed.style.transform = 'translateX(' + translated + 'px)';
 	});
 
 	slideout.on('beforeopen', function () {
-	  fixed.style.transition = 'transform 300ms ease';
-	  fixed.style.transform = 'translateX(256px)';
+		fixed.style.transition = 'transform 300ms ease';
+		fixed.style.transform = 'translateX(256px)';
 	});
 
 	slideout.on('beforeclose', function () {
-	  fixed.style.transition = 'transform 300ms ease';
-	  fixed.style.transform = 'translateX(0px)';
+		fixed.style.transition = 'transform 300ms ease';
+		fixed.style.transform = 'translateX(0px)';
 	});
 
 	slideout.on('open', function () {
-	  fixed.style.transition = '';
+		fixed.style.transition = '';
 	});
 
 	slideout.on('close', function () {
-	  fixed.style.transition = '';
+		fixed.style.transition = '';
 	});
 
 	function close(eve) {
-  eve.preventDefault();
-  slideout.close();
-}
+	eve.preventDefault();
+	slideout.close();
+	}
 
-slideout
-  .on('beforeopen', function() {
-    this.panel.classList.add('panel-open');
-  })
-  .on('open', function() {
-    this.panel.addEventListener('click', close);
-  })
-  .on('beforeclose', function() {
-    this.panel.classList.remove('panel-open');
-    this.panel.removeEventListener('click', close);
-  });
+	slideout
+		.on('beforeopen', function() {
+			this.panel.classList.add('panel-open');
+		})
+		.on('open', function() {
+			this.panel.addEventListener('click', close);
+		})
+		.on('beforeclose', function() {
+			this.panel.classList.remove('panel-open');
+			this.panel.removeEventListener('click', close);
+		});
 
+	slideout.disableTouch();
+
+	//MagnificPopup
 	$('.popup-with-move-anim').magnificPopup({
 		type: 'inline',
 
@@ -79,19 +83,7 @@ slideout
 		}
 	});
 
-	$('.more-button').on('click', function() {
-		$(this).parent().prev('.more-description').slideToggle();
-	});
-
-	slideout.disableTouch();
-
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
-
+	//Calculate Price
 	var selGame = $('#select-game');
 	selGame.on('change', function () {
 		var gameIndex = selGame.val();
@@ -105,8 +97,14 @@ slideout
 		}
 	});
 
+	//Show More description
+	$('.more-button').on('click', function() {
+		$(this).parent().prev('.more-description').slideToggle();
+	});
+
+	//OwlCarousel
 	$(document).ready(function(){
-  	$('.owl-carousel').owlCarousel({
+		$('.owl-carousel').owlCarousel({
 			nav:true,
 			items:1,
 			loop: true,
@@ -114,6 +112,36 @@ slideout
 		});
 	});
 
+	//Countdown
+	// Set the date we're counting down to
+	var countDownDate = new Date("May 17, 2017 20:00:00").getTime();
+	// Update the count down every 1 second
+	var x = setInterval(function() {
+		// Get todays date and time
+		var now = new Date().getTime();
+		// Find the distance between now an the count down date
+		var distance = countDownDate - now;
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		// Display the result in the element with id="demo"
+		document.getElementById("countdown").innerHTML = days + " д. " + hours + " ч. "
+		+ minutes + " м. " + seconds + " с. ";
+		// If the count down is finished, write some text
+		// if (distance < 0) {
+		// 	clearInterval(x);
+		// 	document.getElementById("countdown").innerHTML = "EXPIRED";
+		// 	}
+		}, 1000);
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
