a35a082a4a2ea3cc02f80ea2ca664c8a3578740e
Антон Ильиных < >
2017-04-23 21:06:26 +0700

    carousel

--- games/site/app/js/common.js
+++ games/site/app/js/common.js
@@ -161,7 +161,7 @@ $(function() {
 		margin: 10,
 		lazyload: true,
 		items:1,
-		loop: true,
+		loop: false,
 		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
 		autoWidth: true,
 		autoplay:true,
@@ -254,52 +254,11 @@ $(function() {
 		        bgOpacity: 0.7,
 		        showHideOpacity: true
 		    }
-				console.log($index);
 
 		    // Initialize PhotoSwipe
 		    var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
 		    lightBox.init();
 		});
 	});
-	// var $pswp = $('.pswp')[0];
-	// var image = [];
-	//  $('.thumbnails-container').each( function() {
-	// 		 var $pic     = $(this),
-	// 				 getItems = function() {
-	// 						 var items = [];
-	// 						 $pic.find('a').each(function() {
-	// 								 var $href   = $(this).attr('href'),
-	// 										 $size   = $(this).data('size').split('x'),
-	// 										 $width  = $size[0],
-	// 										 $height = $size[1];
-	// 								 var item = {
-	// 										 src : $href,
-	// 										 w   : $width,
-	// 										 h   : $height
-	// 								 }
-	// 								 items.push(item);
-	// 						 });
-	// 						 return items;
-	// 				 }
-	// 		 var items = getItems();
-	// 		 $.each(items, function(index, value) {
-	// 				 image[index]     = new Image();
-	// 				 image[index].src = value['src'];
-	// 		 });
-	// 		 $pic.on('click', 'figure', function(event) {
-	// 				 event.preventDefault();
-	// 				 var $index = $(this).index();
-	// 				 var options = {
-	// 						 index: $index,
-	// 						 bgOpacity: 0.7,
-	// 						 showHideOpacity: true
-	// 				 }
-	// 				 console.log($index);
-	// 				 console.log(event.target);
-	//
-	// 				 var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-	// 				 lightBox.init();
-	// 		 });
-	//  });
 
 });
